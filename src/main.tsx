75fcef9d9fc73570ee138abbeb627fce5dde8661
hoanggbao00 <kaitou.kurosaki@gmail.com>
2026-03-10 10:54:53 +0700

    chore: update biome configuration, remove tsBuildInfoFile, and adjust button attributes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "@/App";
+import App from "@/app";
 
 // biome-ignore lint/style/noNonNullAssertion: root is always present
 createRoot(document.getElementById("root")!).render(
